1073545e0c776912baaa9f9c52809a670eb27246
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2021-09-21 14:44:38 +0200

    feat: model validations

--- app/models/pet.js
+++ app/models/pet.js
@@ -1,5 +1,11 @@
 import Model, { attr } from '@ember-data/model';
+import { string } from 'yup';
+import YupValidations from 'emberfest-validations/validations/yup';
 
 export default class PetModel extends Model {
+  validations = new YupValidations(this, {
+    name: string().required(),
+  });
+
   @attr('string') name;
 }
--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,14 @@
 import Model, { attr, hasMany } from '@ember-data/model';
+import YupValidations from 'emberfest-validations/validations/yup';
+import { number, array, string } from 'yup';
 
 export default class UserModel extends Model {
+  validations = new YupValidations(this, {
+    name: string().required(),
+    age: number().required(),
+    pets: array().emberDataRelationship(),
+  });
+
   @attr('string') name;
   @attr('number') age;
 
