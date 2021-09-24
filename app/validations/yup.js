688b12a7420fa8ff424a8d816b21c324a3297077
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2021-09-24 17:34:18 +0200

    change locales

--- app/validations/yup.js
+++ app/validations/yup.js
@@ -50,67 +50,64 @@ export default class YupValidations {
 }
 
 const locale =
-  (localeValues = []) =>
-  (key) =>
+  (key, localeValues = []) =>
   (validationParams) => ({
     key,
     path: validationParams.path,
     values: getProperties(validationParams, ...localeValues),
   });
 
-function setupYupValidations() {
-  setLocale({
-    mixed: {
-      default: locale()('field.invalid'),
-      required: locale()('field.required'),
-      oneOf: locale(['values'])('field.oneOf'),
-      notOneOf: locale(['values'])('field.notOneOf'),
-      defined: locale()('field.defined'),
-    },
+setLocale({
+  mixed: {
+    default: locale('field.invalid'),
+    required: locale('field.required'),
+    oneOf: locale('field.oneOf', ['values']),
+    notOneOf: locale('field.notOneOf', ['values']),
+    defined: locale('field.defined'),
+  },
 
-    string: {
-      length: '${path} must be exactly ${length} characters',
-      min: '${path} must be at least ${min} characters',
-      max: '${path} must be at most ${max} characters',
-      matches: '${path} must match the following: "${regex}"',
-      email: '${path} must be a valid email',
-      url: '${path} must be a valid URL',
-      uuid: '${path} must be a valid UUID',
-      trim: '${path} must be a trimmed string',
-      lowercase: '${path} must be a lowercase string',
-      uppercase: '${path} must be a upper case string',
-    },
+  string: {
+    length: '${path} must be exactly ${length} characters',
+    min: '${path} must be at least ${min} characters',
+    max: '${path} must be at most ${max} characters',
+    matches: '${path} must match the following: "${regex}"',
+    email: '${path} must be a valid email',
+    url: '${path} must be a valid URL',
+    uuid: '${path} must be a valid UUID',
+    trim: '${path} must be a trimmed string',
+    lowercase: '${path} must be a lowercase string',
+    uppercase: '${path} must be a upper case string',
+  },
 
-    number: {
-      min: '${path} must be greater than or equal to ${min}',
-      max: '${path} must be less than or equal to ${max}',
-      lessThan: '${path} must be less than ${less}',
-      moreThan: '${path} must be greater than ${more}',
-      positive: '${path} must be a positive number',
-      negative: '${path} must be a negative number',
-      integer: '${path} must be an integer',
-    },
+  number: {
+    min: '${path} must be greater than or equal to ${min}',
+    max: '${path} must be less than or equal to ${max}',
+    lessThan: '${path} must be less than ${less}',
+    moreThan: '${path} must be greater than ${more}',
+    positive: '${path} must be a positive number',
+    negative: '${path} must be a negative number',
+    integer: '${path} must be an integer',
+  },
 
-    date: {
-      min: '${path} field must be later than ${min}',
-      max: '${path} field must be at earlier than ${max}',
-    },
+  date: {
+    min: '${path} field must be later than ${min}',
+    max: '${path} field must be at earlier than ${max}',
+  },
 
-    boolean: {
-      isValue: '${path} field must be ${value}',
-    },
+  boolean: {
+    isValue: '${path} field must be ${value}',
+  },
 
-    object: {
-      noUnknown: '${path} field has unspecified keys: ${unknown}',
-    },
+  object: {
+    noUnknown: '${path} field has unspecified keys: ${unknown}',
+  },
 
-    array: {
-      min: locale(['min'])('field.minLength'),
-      max: '${path} field must have less than or equal to ${max} items',
-      length: '${path} must have ${length} items',
-    },
-  });
-}
+  array: {
+    min: locale(['min'])('field.minLength'),
+    max: '${path} field must have less than or equal to ${max} items',
+    length: '${path} must have ${length} items',
+  },
+});
 
 function extendYup() {
   addMethod(object, 'emberDataRelationship', function () {
@@ -135,4 +132,3 @@ function extendYup() {
 }
 
 extendYup();
-setupYupValidations();
