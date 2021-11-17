3aaca73e56372337929986d25b016664f125b75e
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2021-11-17 22:04:39 +0100

    refactor: use more civilized ways to do conditional validations in relation

--- app/models/pet.js
+++ app/models/pet.js
@@ -1,13 +1,21 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
-import { string } from 'yup';
+import { boolean, string } from 'yup';
 import YupValidations from 'emberfest-validations/validations/yup';
 
 export default class PetModel extends Model {
   validations = new YupValidations(this, {
-    name: string().required(),
+    name: string().when(['isAllergic'], {
+      is: true,
+      then: string().notRequired(),
+      otherwise: string().required(),
+    }),
+    isAllergic: boolean(),
   });
 
   @attr('string') name;
-
   @belongsTo('user') owner;
+
+  get isAllergic() {
+    return this.owner.get('isAllergic');
+  }
 }
